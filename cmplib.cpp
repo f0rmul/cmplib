00bdb338b1da9258af2ca462133735a4641ad0e1
ansa.sequence@gmail.com <ansa.sequence@gmail.com>
2021-10-13 19:50:27 +0300

    Update 1.0 : Adding doxygen documentation Fixed bugs in CMakeLists.txt

--- cmplib.cpp
+++
@@ -1,8 +0,0 @@
-#include "realization/rle_cmp.hpp"
-
-#include <iostream>
-
-void hello()
-{
-    std::cout << "Hello World!" << '\n';
-}
--- realization/rle_cmp.hpp
+++ realization/rle_cmp.hpp
@@ -5,5 +5,15 @@
 #ifndef CMPLIB_RLE_CMP_HPP
 #define CMPLIB_RLE_CMP_HPP
 
+#include <iostream>
+
+class Hello
+{
+public:
+    void print()
+    {
+        std::cout << "Hello" << '\n';
+    }
+};
 
 #endif //CMPLIB_RLE_CMP_HPP
